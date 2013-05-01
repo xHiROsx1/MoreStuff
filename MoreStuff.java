e3f6e196fc71569ebbada3d952081222cdbff4e0
xHiROsx1 <kslrtips2@gmail.com>
2013-04-30 23:42:25 -0300

    Update MoreStuff.java

--- MoreStuff.java
+++ MoreStuff.java
@@ -3,10 +3,14 @@ package co.thelostrealms.me.morestuff;
 //Created by HiROs from Thelostrealms.co
 //Free to use and copy.
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MoreStuff extends JavaPlugin {
-  String version = "1.0";
+	String version = "1.0.1";
 	int errors = 0;
 	@Override
 	public void onEnable() {
@@ -15,5 +19,21 @@ public class MoreStuff extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		this.getLogger().info("MoreStuff has been disable. Errors Found: " + errors + ".");
+	}
+	//Commands
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		String noperms = ChatColor.DARK_RED + "MoreStuff does not let you do that!";
+		//Heal Command
+		if(label.equalsIgnoreCase("heal") || label.equalsIgnoreCase("h")) {
+			if(!player.hasPermission("morestuff.heal")) {
+				player.sendMessage(noperms);
+				return false;
+			}
+			player.setHealth(0);
+			player.sendMessage(ChatColor.GREEN + "You have healed yourself!");
+		}
+		
+		return true;
 	}
 }
